0cfdc8aa9fd744150dd861608772a8ace651b108
Shaffaf <parascent@gmail.com>
2019-11-06 22:19:49 +0500

    enhanced general CRUD screen a bit

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,14 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'TasksController@all')->name('home')->middleware('auth');
-Route::get('/tasks', 'TasksController@all')->name('tasks')->middleware('auth');
-Route::get('/tasks/{id}', 'TasksController@single')->name('singleTask')->middleware('auth');
-Route::post('/tasks', 'TasksController@add')->name('addTask')->middleware('auth');
-Route::patch('/tasks/{id}', 'TasksController@edit')->name('editTask')->middleware('auth');
-Route::patch('/tasks/{id}/markdone', 'TasksController@markDone')->name('markDoneTask')->middleware('auth');
-Route::delete('/tasks/{id}', 'TasksController@delete')->name('deleteTask')->middleware('auth');
+Route::middleware(['auth'])->group(function(){
+	Route::get('/home', 'TasksController@all')->name('home');
+	Route::get('/tasks', 'TasksController@all')->name('tasks');
+	Route::get('/tasks/{id}', 'TasksController@single')->name('singleTask');
+	Route::post('/tasks', 'TasksController@add')->name('addTask');
+	Route::patch('/tasks/{id}', 'TasksController@edit')->name('editTask');
+	Route::patch('/tasks/{id}/markdone', 'TasksController@markDone')->name('markDoneTask');
+	Route::delete('/tasks/{id}', 'TasksController@delete')->name('deleteTask');
+});
+
 
